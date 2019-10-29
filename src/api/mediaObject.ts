81a7cc0640d1d74321a8c10b2adefa9bfa3f2f7b
Johan Nyman <johan@nytamin.se>
2019-10-29 11:18:00 +0100

    fix: update typings from Core

--- src/api/mediaObject.ts
+++ src/api/mediaObject.ts
@@ -59,10 +59,19 @@ export enum FieldOrder {
 
 export interface MediaInfo {
 	name: string
-	field_order: FieldOrder
-	scenes: number[]
-	streams: MediaStream[]
-	format: MediaFormat
+	field_order?: FieldOrder
+	scenes?: number[]
+	blacks?: Array<Anomaly>
+	freezes?: Array<Anomaly>
+	streams?: MediaStream[]
+	format?: MediaFormat
+	timebase?: number
+}
+
+export interface Anomaly {
+	start: number
+	duration: number
+	end: number
 }
 
 export interface MediaAttachment {
@@ -73,17 +82,29 @@ export interface MediaAttachment {
 }
 
 export interface MediaObject {
+	/** The playable reference (CasparCG clip name, quantel GUID, etc) */
 	mediaId: string
+
+	/** Media object file path relative to playout server */
 	mediaPath: string
+	/** Media object size in bytes */
 	mediaSize: number
+	/** Timestamp when the media object was last updated */
 	mediaTime: number
+	/** Info about media content. If undefined: inficates that the media is NOT playable (could be transferring, or a placeholder)  */
 	mediainfo?: MediaInfo
 
+	/** Thumbnail file size in bytes */
 	thumbSize: number
+	/** Thumbnail last updated timestamp */
 	thumbTime: number
 
+	/** Preview file size in bytes */
 	previewSize?: number
+	/** Thumbnail last updated timestamp */
 	previewTime?: number
+	/** Preview location */
+	previewPath?: string
 
 	cinf: string // useless to us
 	tinf: string // useless to us
