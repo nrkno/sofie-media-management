510dd31a38096953b27beed20afc342f1623ccf9
Julian Waller <git@julusian.co.uk>
2019-11-13 16:33:53 +0000

    feat: Add crosscompile build script

--- copy-natives.js
+++ copy-natives.js
@@ -5,16 +5,19 @@ const fs = require('fs-extra')
 
 const arch = os.arch()
 const platform = os.platform()
+const prebuildType = process.argv[2] || `${platform}-${arch}`
 
 function isFileForPlatform(filename) {
-    if (filename.indexOf(path.join('prebuilds', `${platform}-${arch}`)) !== -1) {
+    if (filename.indexOf(path.join('prebuilds', prebuildType)) !== -1) {
         return true
     } else {
         return false
     }
 }
 
-console.log('Running in', __dirname)
+console.log('Running in', __dirname, 'for', prebuildType)
+
+console.log(process.argv[2])
 
 find.file(/\.node$/, path.join(__dirname, 'node_modules'), (files) => {
     files.forEach(fullPath => {
