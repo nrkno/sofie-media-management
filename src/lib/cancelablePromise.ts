adbce203882197f283775b5899ee95e3eaefdd1d
Julian Waller <git@julusian.co.uk>
2019-11-13 14:52:21 +0000

    chore: update dependencies

--- src/lib/cancelablePromise.ts
+++ src/lib/cancelablePromise.ts
@@ -28,6 +28,9 @@ export class CancelablePromise<T> implements Promise<T> {
 	): Promise<T | TResult> {
 		return this._basePromise.catch(onrejected)
 	}
+	finally(onfinally?: (() => void) | null | undefined): Promise<T> {
+		return this._basePromise.finally(onfinally)
+	}
 	[Symbol.toStringTag]: 'Promise'
 
 	cancel(): void {
